ccae4caac1b1a9c152c952fec526934ef68a57f8
HectorVybz <A01713794@tec.mx>
2025-05-22 22:19:09 -0600

    Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include "Electronico.h"
+#include "Ropa.h"
+#include "Carrito.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Electronico* laptop = new Electronico("Laptop ASUS", 15000.0, 24);
+    Ropa* camiseta = new Ropa("Camiseta Negra", 299.0, "M");
+
+    Carrito carrito;
+    carrito.agregarProducto(laptop);
+    carrito.agregarProducto(camiseta);
+
+    cout << "Productos en el carrito:" << endl;
+    carrito.mostrarProductos();
+
+    cout << "Total a pagar: $" << carrito.calcularTotal() << endl;
+
+    delete laptop;
+    delete camiseta;
+
+    return 0;
+}
